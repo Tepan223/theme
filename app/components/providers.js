d8597c091afdf1b8ee4495c77198fbca34c379cf
Stevanus564 <stevanus@summitglobal.id>
2026-05-12 16:00:36 +0700

    setup theme with next-themes and ant switch

---
+++ app/components/providers.js
@@ -0,0 +1,19 @@
+"use client"
+
+import { ThemeProvider } from "next-themes"
+
+export default function Providers({ children }) {
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+    )
+}
+
+
+// Install next-themes!!!
