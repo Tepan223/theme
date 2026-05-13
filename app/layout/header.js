037df8bf8c3abe8e437a73e01ef9474073fe480d
Stevanus564 <stevanus@summitglobal.id>
2026-05-13 16:32:14 +0700

    update

--- app/layout/header.js
+++ app/layout/header.js
@@ -4,7 +4,11 @@ import styles from './styles/Header.module.css';
 const Header = () => {
     return (
         <div className={styles.header}>
-
+            <a href="#" className={styles.link}>Home</a>
+            <a href="#" className={styles.link}>About</a>
+            <a href="#" className={styles.link}>Project</a>
+            <a href="#" className={styles.link}>Skill</a>
+            <a href="#" className={styles.link}>Contact</a>
         </div>
     );
 };
